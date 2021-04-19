a09208e619296d4b301e71ec4a1dcf4b4ab76d54
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-04-19 05:55:40 +0530

    Update client.py

--- tg/client.py
+++ tg/client.py
@@ -1,5 +1,9 @@
 from decouple import config
-from telethon import Button, TelegramClient, events
+#from telethon import Button, TelegramClient, events
+from pyrogram import (
+    Client,
+    __version__
+)
 import logging
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -9,4 +13,4 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
-tgbot = TelegramClient("kensur", APP_ID, API_HASH).start(bot_token=BOT_TOKEN)
+tgbot = Client(session_name="kensur", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
