6332c365792ed079108abf211fc38b888ff1c237
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-04-19 05:41:53 +0530

    Create client.py

---
+++ tg/client.py
@@ -0,0 +1,14 @@
+from decouple import config
+from telethon import Button, TelegramClient, events
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
+
+APP_ID = config("APP_ID", default=None, cast=int)
+API_HASH = config("API_HASH", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=None)
+
+tgbot = TelegramClient("kensur", APP_ID, API_HASH)
+
+tgbot.start(bot_token=BOT_TOKEN)
