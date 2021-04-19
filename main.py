2e3416e56d7fca7ccc2006494fa0976963c46870
iam-pro <iam-pro@users.noreply.github.com>
2021-04-19 16:29:52 +0000

    pylint: auto fixes

--- main.py
+++ main.py
@@ -90,8 +90,13 @@ async def respond(request: Request):
 
 PORT = config("PORT")
 if __name__ == "__main__":
-    fastap = threading.Thread(target=uvicorn.run("main:app", host="0.0.0.0", port=int(PORT), log_level="info"), daemon=True)
+    fastap = threading.Thread(
+        target=uvicorn.run(
+            "main:app", host="0.0.0.0", port=int(PORT), log_level="info"
+        ),
+        daemon=True,
+    )
     fastap.start()
     tgbot.start(bot_token=BOT_TOKEN)
     tgbot.run_until_disconnected()
-    #uvicorn.run("main:app", host="0.0.0.0", port=int(PORT), log_level="info")
+    # uvicorn.run("main:app", host="0.0.0.0", port=int(PORT), log_level="info")
