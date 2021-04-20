e02776af690dc6e693873a451b4b4faf383dc5d7
Error <error@notavailable.live>
2021-04-20 08:18:19 +0530

    Update tg.py

--- tg.py
+++ tg.py
@@ -11,6 +11,6 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
-tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH)
+tgbot = TelegramClient("fuckheroku", api_id=APP_ID, api_hash=API_HASH)
 
 tgbot.start(bot_token=BOT_TOKEN)
