f02574bc22e43fdfd744f4d6d5f03424d7105db1
ProgrammingError <ProgrammingError@users.noreply.github.com>
2021-04-20 02:16:01 +0000

    pylint: auto fixes

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
 # tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
-tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(
+    bot_token=BOT_TOKEN
+)
 # app = Flask("Kek")
 app = FastAPI(debug=True)
 print("Successfully deployed!")
