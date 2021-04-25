93ba7b0fc034f6a5d534fa242bba28e0eb6649cb
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-04-25 19:50:05 +0530

    Update flaskish.py

--- flaskish.py
+++ flaskish.py
@@ -1,24 +1,41 @@
-import asyncio
-import threading
+import io
+import os
+import logging
+import random
+import sys
+import subprocess
 import traceback
+import threading
+import uvicorn
+import requests
+from decouple import config
+#from pyrogram import (
+#    Client,
+#    __version__
+#)
+BOT_TOKEN = config("TOKEN")
+from fastapi import FastAPI,Request
+#from flask import Flask, request, Response
+print("Successfully deployed!")
+app = FastAPI(debug=True)
+API = f'https://api.telegram.org/bot{BOT_TOKEN}/'
+def post_tg(chat, message, parse_mode):
+    """Send message to desired group"""
+    response = requests.post(
+        API + "sendMessage",
+        params={
+            "chat_id": chat,
+            "text": message,
+            "parse_mode": parse_mode,
+            "disable_web_page_preview": True}).json()
+    return response
 
-from aiogram.utils import executor
-from flask import Flask, request
-
-from tg import bot
-
-loop = asyncio.get_event_loop()
-
-app = Flask(__name__)
-
-
-async def send_msg(id, msg):
-    await bot.send_message(-1001237141420, msg, disable_web_page_preview=True)
-
-
-@app.route("/webhook", methods=["POST"])
-def index():
-    result = request.get_json()
+@app.post('/webhook')
+async def respond(request: Request):
+    result = await request.json()
+#    await tgbot.start(bot_token=BOT_TOKEN)
+    #print(request.json)
+    d_form = "%d/%m/%y || %H:%M"
     try:
         # check_s = result["check_suite"]
         # umm = check_s["app"]["head_commit"]
@@ -28,12 +45,13 @@ def index():
                 rng = 10
             for x in range(rng):
                 commit = result["commits"][x]
+                commit_ts = commit["timestamp"]
                 if len((commit["message"])) > 300:
                     commit_msg = (commit["message"]).split("\n")[0]
                 else:
                     commit_msg = commit["message"]
-                text = f"**{commit_msg}**\n[{commit['id'][:7]}]({commit['url']})\n{commit['author']['name']} <{commit['author']['email']}>"
-                loop.run_until_complete(send_msg(-1001237141420, text))
+                text = f"**{commit_msg}**\n[{commit['id'][:7]}]({commit['url']})\n**Commited at**{commit_ts.stfrtime(d_form)}\n{commit['author']['name']} <{commit['author']['email']}>"
+                post_tg(-1001237141420, text, parse_mode="markdown")
         elif result.get("pull_request"):
             pr_action = result["action"]
             pr = result["pull_request"]
@@ -44,14 +62,16 @@ def index():
             pull_ts = pr["created_at"]
             pull_pusher = pr["user"]["login"]
             if pr_action == "opened":
-                text = f"**Opened Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts}\n[Commits]({pull_commits})"
+                text = f"**Opened Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts.stfrtime(d_form)}\n[Commits]({pull_commits})"
             elif pr_action == "closed":
-                text = f"**Closed Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts}\n[Commits]({pull_commits})"
+                text = f"**Closed Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts.stfrtime(d_form)}\n[Commits]({pull_commits})"
             else:
-                text = f"**Reopened Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts}\n[Commits]({pull_commits})"
+                text = f"**Reopened Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts.stfrtime(d_form)}\n[Commits]({pull_commits})"
 
-            loop.run_until_complete(send_msg(int("-1001237141420"), text))
-
+             post_tg(-1001237141420, text, parse_mode="markdown")
+		elif data.get('action') == "started":
+			text = f"🌟 {data['sender']['html_url']} {data['sender']['login']} gave a star to [{data['repository']['name']}]({data['repository']['html_url']})"
+			post_tg(-1001237141420, text, parse_mode="markdown")
         else:
             umm = result["head_commit"]
             commit_msg = umm["message"]
@@ -66,5 +86,4 @@ def index():
 
 
 if __name__ == "__main__":
-    threading.Thread((app.run(host="0.0.0.0", debug=True)), daemon=True).start()
-    executor.start(dp, broadcaster())
+    uvicorn.run("main:app", host="0.0.0.0", port=int(PORT), log_level="info")
