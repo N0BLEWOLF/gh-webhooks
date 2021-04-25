26dbe9bc5b01101692ece2d57bbd1aaf84bde9c2
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-04-25 18:06:29 +0530

    Update tg.py

--- tg.py
+++ tg.py
@@ -2,6 +2,7 @@ import logging
 
 from decouple import config
 from telethon import TelegramClient
+from aiogram import Bot, Dispatcher, types
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
@@ -11,6 +12,6 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
-tgbot = TelegramClient("fuckheroku", api_id=APP_ID, api_hash=API_HASH)
-
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.markdown)
+tgbot = Dispatcher(bot)
 # tgbot.start(bot_token=BOT_TOKEN)
