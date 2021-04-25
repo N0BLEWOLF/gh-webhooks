67c5b36d17877a6d3e5cdd992ceb546d92fb18e8
ProgrammingError <ProgrammingError@users.noreply.github.com>
2021-04-25 06:04:50 +0000

    pylint: auto fixes

--- flaskish.py
+++ flaskish.py
@@ -1,13 +1,15 @@
+import asyncio
 import threading
 import traceback
-import asyncio
-from decouple import config
-from flask import Flask, jsonify
+
+from flask import Flask
+
 from tg import BOT_TOKEN, tgbot
 
 loop = asyncio.get_event_loop()
 
 app = Flask(__name__)
+
 
 @app.route("/webhook", methods=["POST"])
 def index(res):
@@ -42,10 +44,10 @@ def index(res):
                 text = f"**Closed Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts}\n[Commits]({pull_commits})"
             else:
                 text = f"**Reopened Pull Request**\nBy: {pull_pusher}\n[{pull_t}]({pull_r})\n**Timestamp**: {pull_ts}\n[Commits]({pull_commits})"
-            
-            loop.run_until_complete(await tgbot.send_message(
-                int("-1001237141420"),
-                text))
+
+            loop.run_until_complete(
+                await tgbot.send_message(int("-1001237141420"), text)
+            )
 
         else:
             umm = result["head_commit"]
@@ -55,16 +57,17 @@ def index(res):
             commit_timestamp = umm["timestamp"]
             committer_name = umm["author"]["username"]
             committer_mail = umm["author"]["email"]
-            loop.run_until_complete(await tgbot.send_message(
-                -1001237141420,
-                f"Commit: [{commit_id}]({commit_url})\nMessage: **{commit_msg}**\nTimeStamp: `{commit_timestamp}`\nCommiter: {committer_name} <{committer_mail}>",
-            ))
-    except:
+            loop.run_until_complete(
+                await tgbot.send_message(
+                    -1001237141420,
+                    f"Commit: [{commit_id}]({commit_url})\nMessage: **{commit_msg}**\nTimeStamp: `{commit_timestamp}`\nCommiter: {committer_name} <{committer_mail}>",
+                )
+            )
+    except BaseException:
         traceback.print_exc()
 
+
 if __name__ == "__main__":
-    threading.Thread((
-		app.run(host='0.0.0.0',debug=True)
-    ),daemon=True).start()
+    threading.Thread((app.run(host="0.0.0.0", debug=True)), daemon=True).start()
     tgbot.start(BOT_TOKEN)
     tgbot.run_until_disconnected()
