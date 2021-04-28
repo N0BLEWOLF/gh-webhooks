2963e7d421260d21779e17168b0b133be77e83c9
Error <error@notavailable.live>
2021-04-28 21:57:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,21 +4,8 @@ import threading
 from sanic import Sanic
 from sanic.response import json
 from telethon import TelegramClient
-
+from client import tgbot
 app = Sanic(name="MC")
-
-APP_ID = 3512712
-API_HASH = "4ff3477ad1d19aad032dc015f5d05a52"
-TOKEN = "1767375736:AAF_oQ0mN8T2Zu8DtYZAUeX9ou5MyBQw_HQ"
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
-
-tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(
-    bot_token=TOKEN
-)
-
 
 @app.route("/omk")
 async def test(request):
@@ -34,4 +21,3 @@ async def fuck(request):
 
 if __name__ == "__main__":
     threading.Thread(target=app.run(host="0.0.0.0", port=8000), daemon=True).start()
-    tgbot.run_until_disconnected()
