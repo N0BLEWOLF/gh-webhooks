87e77856c3c3f89c8930651716037282e4aa95ce
Error <error@notavailable.live>
2021-04-28 22:20:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,14 @@ from client import config, tgbot
 
 app = FastAPI(debug=True)
 
+print("Go Injoi!")
 
-@app.get("/omk")
+@app.get("/")
 async def test(request: Request):
     return {"hello": "world"}
 
 
-@app.get("/")
+@app.get("/omk")
 async def fuck(request: Request):
     om = await tgbot.send_message(-1001237141420, "TEST")
     print(om)
