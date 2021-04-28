7fffa0464fd395554241261c3bfe00c78dc4512a
ProgrammingError <ProgrammingError@users.noreply.github.com>
2021-04-28 05:50:43 +0000

    pylint: auto fixes

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+import logging
+import threading
+
 from sanic import Sanic
-import threading, logging
 from sanic.response import json
 from telethon import TelegramClient
 
@@ -21,12 +23,15 @@ tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(
 @app.route("/omk")
 async def test(request):
     return json({"hello": "world"})
+
+
 @app.route("/")
 async def fuck(request):
     om = await tgbot.send_message("The_Masoom_Bachha", "Hi")
     print(om)
     return json({"msg": "MC"})
 
+
 if __name__ == "__main__":
-    threading.Thread(target=app.run(host="0.0.0.0", port=8000),daemon=True).start()
-    threading.Thread(target=tgbot.run_until_disconnected(),daemon=True).start()
+    threading.Thread(target=app.run(host="0.0.0.0", port=8000), daemon=True).start()
+    threading.Thread(target=tgbot.run_until_disconnected(), daemon=True).start()
