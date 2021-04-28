05532ea55195499872cc13503f26d5f5b33bc4b7
Error <error@notavailable.live>
2021-04-28 22:33:23 +0530

    Update client.py

--- client.py
+++ client.py
@@ -10,9 +10,8 @@ logging.basicConfig(
 APP_ID = config("API_ID")
 API_HASH = config("API_HASH")
 BOT_TOKEN = config("TOKEN")
-tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH).start(
-    bot_token=BOT_TOKEN
-)
+tgbot = TelegramClient("kensur", api_id=APP_ID, api_hash=API_HASH)
 print("OK?")
 loop = asyncio.get_event_loop()
+loop.run_until_complete(tgbot.start(bot_token=BOT_TOKEN))
 loop.run_until_complete(tgbot.run_until_disconnected())
