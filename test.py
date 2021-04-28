2b61660143af9eb85cc9fbd0def6753881d8af42
Error <error@notavailable.live>
2021-04-28 22:17:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-from sanic import Sanic
-from sanic.response import json
-
+from fastapi import FastAPI, Request
+import uvicorn
 from client import config, tgbot
 
-app = Sanic(name="MC")
+app = FastAPI(debug=True)
+
+@app.get("/omk")
+async def test(request: Request):
+    return ({"hello": "world"})
 
 
-@app.route("/omk")
-async def test(request):
-    return json({"hello": "world"})
-
-
-@app.route("/")
-async def fuck(request):
+@app.get("/")
+async def fuck(request: Request):
     om = await tgbot.send_message(-1001237141420, "TEST")
     print(om)
-    return json({"msg": "MC"})
+    return ({"msg": "MC"})
 
 
 PORT = config("PORT")
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(PORT))
+    uvicorn.run(
+        "test:app", host="0.0.0.0", port=int(PORT), log_level="info", reload=False
+    )
