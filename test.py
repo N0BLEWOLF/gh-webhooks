29db96fb4bc1dbe9bd6878dc3be2770a871f6594
Caleb Noble <74255178+iam-pro@users.noreply.github.com>
2021-04-29 06:50:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI, Request
 
 from client import config, tgbot
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, executor
+from decouple import config
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
+APP_ID = config("API_ID")
+API_HASH = config("API_HASH")
+BOT_TOKEN = config("TOKEN")
+tgbot = Bot(token=BOT_TOKEN)
+
+print("OK?")
+
+omkk = asyncio.get_event_loop()
+
+dp = Dispatcher(tgbot)
 
 app = FastAPI(debug=True)
 
@@ -20,5 +39,6 @@ async def fuck(request: Request):
 
 
 PORT = config("PORT")
-
-# uvicorn.run("test:app", host="0.0.0.0", port=int(PORT), log_level="info", reload=False)
+if __name__ == "__main__":
+    threading.Thread(target=executor.start_polling, args=(dp, skip_updates=True))
+    uvicorn.run("test:app", host="0.0.0.0", port=int(PORT), log_level="info", reload=False)
