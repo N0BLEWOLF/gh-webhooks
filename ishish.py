88c7a2dc279517e5d6a6ae7e56ccb0f1b1112f10
Error <error@notavailable.live>
2021-05-01 11:18:56 +0530

    Update ishish.py

--- ishish.py
+++ ishish.py
@@ -94,7 +94,7 @@ async def respond(request):
                                 Button.url("View Commit", {commit["url"]}),
                                 Button.url(
                                     "Commited By",
-                                    result['sender']['html_url']"
+                                    str(result['sender']['html_url'])
                                 ),
                             ]
                         ],
